5c089a41d07198b968a980551bdcc70cdad189b3
muyul639 <youngbum050@naver.com>
2026-05-25 20:29:53 +0900

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,17 +1,14 @@
 from flask import Flask
 from threading import Thread
-import os
 
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "봇이 정상적으로 작동 중입니다!"
+    return "24/7 Alive"
 
 def run():
-    # 렌더가 지정한 PORT를 최우선으로 사용합니다.
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
 
 def keep_alive():
     t = Thread(target=run)
