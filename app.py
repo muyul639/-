0d7719396f980ac83f78dadde333f777b8f7d30e
muyul639 <youngbum050@naver.com>
2026-05-29 12:43:57 +0900

    app.py

--- app.py
+++ app.py
@@ -95,11 +95,13 @@ class BuckshotView(discord.ui.View):
         desc = f"{self.game.load_info_text}\n"
         desc += f"📦 **남은 총탄:** {remaining_shells}발\n"
         desc += f"---------------------------------------\n\n"
-        desc += f"{action_text}\n\n"
+        desc += f"{action_text}"
 
         embed = discord.Embed(title="-----------------🏴‍☠️------------------", 
                               description=desc, 
                               color=discord.Color.dark_red())
+        
+        embed.add_field(name="\u200b", value="\u200b", inline=False)
         
         # 각 플레이어의 정보를 '필드(Field)'로 분리하여 추가합니다.
         for i, p in enumerate(self.game.players):
